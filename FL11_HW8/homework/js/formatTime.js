9a93cdc8c8a750ab880c464e952745bec4728069
Dmytro Gnatyshyn <42860905+DG1202@users.noreply.github.com>
2019-07-14 16:45:43 +0300

    upd  code format

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,8 +1,8 @@
 function formatTime(minutes) {
-	const restMinutes = minutes % 60;
-	const hours = Math.floor((minutes % 1440) / 60);
-	const days = Math.floor(minutes / 1440) ;
-	return `${days} day(s) ${hours} hour(s) ${restMinutes} minute(s).`;
+  const restMinutes = minutes % 60;
+  const hours = Math.floor((minutes % 1440) / 60);
+  const days = Math.floor(minutes / 1440) ;
+  return `${days} day(s) ${hours} hour(s) ${restMinutes} minute(s).`;
 }
 
 formatTime(120);
