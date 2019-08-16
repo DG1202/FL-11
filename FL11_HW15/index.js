a8b8cf58baf9ebb31ecf0b34b6db6f96efa0c05b
Dmytro Gnatyshyn <di1202@ukr.net>
2019-08-16 23:56:53 +0300

    Add HW14

---
+++ FL11_HW15/index.js
@@ -0,0 +1,54 @@
+class Hamburger{
+	constructor(type,calories,secretIngridient) {
+		this.type = type;
+
+		let _calories = calories,
+				_cheese = 0,
+				_tomato = 0,
+				_secretIngridient = secretIngridient,
+				_timesBite = 0;
+
+		this.getCalories = () => _calories;
+		this.setCalories = (newCaloriesValue) =>{
+				_calories = newCaloriesValue;
+		};
+		this.addCheese = () => {
+			if(_cheese < 1 && !_timesBite){
+				_cheese++;
+				this.setCalories(this.getCalories() + 120)
+			}else if(_timesBite){
+				console.log('Sorry, you cannot add cheese')
+			}else{
+				console.log('Sorry, you can add cheese only once')
+			}
+		};
+		this.addTomato = () => {
+			if(_tomato < 2 && !_timesBite){
+				_tomato++;
+				this.setCalories(this.getCalories() + 20)
+			}else if(_timesBite){
+				console.log('Sorry, you cannot add tomato')
+			}else{
+				console.log('Sorry, you can add tomato only twice')
+			}
+		};
+		this.addSecretIngredient = () => {
+			if(!_tomato && !_cheese && _secretIngridient < 1 && !_timesBite){
+				_secretIngridient++;
+				this.setCalories(this.getCalories() + 100)
+			}else if(_timesBite){
+				console.log('Sorry, you cannot add tomato');
+			}else if(!_tomato && !_cheese && !!_secretIngridient){
+				console.log('Sorry, you can add secret ingridient only before another ingridient')
+			}else{
+				console.log('Sorry, you can add secret ingridient only once')
+			}
+		};
+		this.bite = () => {
+			_timesBite++;
+		};
+		this.info = () => {
+			console.log(`${this.type} hamburger: ${!!_secretIngridient ? 'with secret ingridient,': ''}${!!_cheese ?' with cheese,': ''}${!!_tomato ? ' with ' + _tomato + ' tomato,': ''}${!! _timesBite ? ' is bit ' + _timesBite + ' times. ' : "didn't bit yet. "}Total calories: ${_calories}`)
+		}
+	}
+}
