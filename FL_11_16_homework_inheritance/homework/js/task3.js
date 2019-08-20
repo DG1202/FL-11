9a8d12a6b2d4effa942fd3c3238d325612ac3770
Dmytro Gnatyshyn <42860905+DG1202@users.noreply.github.com>
2019-08-20 23:24:39 +0300

    final code formatting

--- FL_11_16_homework_inheritance/homework/js/task3.js
+++ FL_11_16_homework_inheritance/homework/js/task3.js
@@ -1,69 +1,67 @@
 function Pockemon() {
-	this.element = 'fire';
-	this.specie = 'Flame Pokémon';
-	this.fly = false;
-	this.nextLevel = Charizard;
+  this.element = 'fire';
+  this.specie = 'Flame Pokémon';
+  this.fly = false;
+  this.nextLevel = Charizard;
 }
 
 Pockemon.prototype = {
-	getType: function () {
-		return this.element;
-	},
-	getSpecie: function () {
-		return this.specie;
-	},
-	canFly: function () {
-		return this.fly;
-	},
-	getPokemonType: function () {
-		return this.pokemonType.name;
-
-	},
-	evolve: function () {
-		if (this.nextLevel !== this.pokemonType) {
-			const NewLevel = this.nextLevel;
-			return new NewLevel();
-		}
-		return this;
-	}
+  getType: function() {
+    return this.element;
+  },
+  getSpecie: function() {
+    return this.specie;
+  },
+  canFly: function() {
+    return this.fly;
+  },
+  getPokemonType: function() {
+    return this.pokemonType.name;
+  },
+  evolve: function() {
+    if(this.nextLevel !== this.pokemonType) {
+      const NewLevel = this.nextLevel;
+	return new NewLevel();
+    }
+    return this;
+  }
 };
 
 function Charmander() {
-	Pockemon.call(this);
-	this.specie = 'Lizard Pokémon';
-	this.nextLevel = Charmeleon;
-	this.pokemonType = Charmander;
+  Pockemon.call(this);
+  this.specie = 'Lizard Pokémon';
+  this.nextLevel = Charmeleon;
+  this.pokemonType = Charmander;
 }
 function Charmeleon() {
-	Pockemon.call(this);
-	this.pokemonType = Charmeleon;
-
+  Pockemon.call(this);
+  this.pokemonType = Charmeleon;
 }
 function Charizard() {
-	Pockemon.call(this);
-	this.fly = true;
-	this.pokemonType = Charizard;
+  Pockemon.call(this);
+  this.fly = true;
+  this.pokemonType = Charizard;
 }
 function Raichu() {
-	Pockemon.call(this);
-	this.specie = 'Mouse Pokémon';
-	this.nextLevel = Raichu;
-	this.pokemonType = Raichu;
+  Pockemon.call(this);
+  this.specie = 'Mouse Pokémon';
+  this.nextLevel = Raichu;
+  this.pokemonType = Raichu;
 }
 function Pichu() {
-	Raichu.call(this);
-	this.nextLevel = Pikachu;
-	this.pokemonType = Pichu;
+  Raichu.call(this);
+  this.nextLevel = Pikachu;
+  this.pokemonType = Pichu;
 }
 function Pikachu() {
-	Raichu.call(this);
-	this.pokemonType = Pikachu;
+  Raichu.call(this);
+  this.pokemonType = Pikachu;
 }
 
 const pockemons = [Charmander, Charmeleon, Charizard, Pichu, Pikachu, Raichu];
 pockemons.forEach(pockemon => {
-	pockemon.prototype = Object.create(Pockemon.prototype);
-	pockemon.prototype.constructor = pockemon;
+  pockemon.prototype = Object.create(Pockemon.prototype);
+  pockemon.prototype.constructor = pockemon;
 });
 
 const charmander = new Charmander();
