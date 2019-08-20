8bca3889c715139ce79ccf3b0a1e3d1fc7ee6a28
Dmytro Gnatyshyn <di1202@ukr.net>
2019-08-20 22:34:12 +0300

    add HW16

---
+++ FL_11_16_homework_inheritance/homework/js/task3.js
@@ -0,0 +1,110 @@
+function Pockemon() {
+	this.element = 'fire';
+	this.specie = 'Flame Pokémon';
+	this.fly = false;
+	this.nextLevel = Charizard;
+}
+
+Pockemon.prototype.getType = function () {
+	return this.element;
+};
+
+Pockemon.prototype.getSpecie = function () {
+	return this.specie;
+};
+
+Pockemon.prototype.canFly = function () {
+	return this.fly;
+};
+Pockemon.prototype.getPokemonType = function () {
+	return this.pokemonType.name;
+};
+
+Pockemon.prototype.evolve = function () {
+	if(this.nextLevel !== this.pokemonType){
+		const NewLevel = this.nextLevel;
+		return new NewLevel();
+	}
+	return this;
+};
+
+
+function Charmander() {
+	Pockemon.call(this);
+	this.specie = 'Lizard Pokémon';
+	this.nextLevel = Charmeleon;
+	this.pokemonType = Charmander;
+}
+function Charmeleon() {
+	Pockemon.call(this);
+	this.pokemonType = Charmeleon;
+
+}
+function Charizard() {
+	Pockemon.call(this);
+	this.fly = true;
+	this.pokemonType = Charizard;
+}
+Charmander.prototype = Object.create(Pockemon.prototype);
+Charmeleon.prototype = Object.create(Pockemon.prototype);
+Charizard.prototype = Object.create(Pockemon.prototype);
+Charmander.prototype.constructor = Charmander;
+Charmeleon.prototype.constructor = Charmeleon;
+Charizard.prototype.constructor = Charizard;
+
+function Raichu() {
+	Pockemon.call(this);
+	this.specie = 'Mouse Pokémon';
+	this.nextLevel = Raichu;
+	this.pokemonType = Raichu;
+}
+function Pichu() {
+	Raichu.call(this);
+	this.nextLevel = Pikachu;
+	this.pokemonType = Pichu;
+}
+function Pikachu() {
+	Raichu.call(this);
+	this.pokemonType = Pikachu;
+}
+
+Pichu.prototype = Object.create(Pockemon.prototype);
+Pikachu.prototype = Object.create(Pockemon.prototype);
+Raichu.prototype = Object.create(Pockemon.prototype);
+Pichu.prototype.constructor = Pichu;
+Pikachu.prototype.constructor = Pikachu;
+Raichu.prototype.constructor = Raichu;
+
+const charmander = new Charmander();
+const charmeleon = new Charmeleon();
+const charizard = new Charizard();
+
+console.log(charmander.getType()); // -> “Fire”
+console.log(charmander.getType() === charmeleon.getType()); // -> true
+console.log(charmeleon.getType() === charizard.getType()); // -> true
+
+console.log(charmander.evolve().constructor === Charmeleon); // -> true
+console.log(charmeleon.evolve().constructor === Charizard); // -> true
+
+console.log(charmander.getSpecie()); // -> “Lizard Pokémon”
+console.log(charmeleon.getSpecie()); // -> “Flame Pokémon”
+console.log(charizard.getSpecie() === charmeleon.getSpecie()); // -> true
+
+console.log(charmander.canFly()); // -> false
+console.log(charmander.canFly() === charmeleon.canFly()); // -> true
+console.log(charizard.canFly()); // -> true
+
+
+const pichu = new Pichu();
+console.log(pichu.getPokemonType()); // => Pichu
+
+const pikachu = pichu.evolve();
+console.log(pikachu.getPokemonType()); // Pikachu
+console.log(pikachu.constructor === Pikachu); // true
+
+const raichu = pikachu.evolve();
+console.log(raichu.getPokemonType()); // Raichu
+console.log(raichu.constructor === Raichu); // true
+
+const raichu2 = raichu.evolve(); // return raichu back as it's maximum level
+console.log(raichu2 === raichu);
