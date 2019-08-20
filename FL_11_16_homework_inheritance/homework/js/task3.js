706cf7bff6a9850b327594a23cd3c8d4188b7b26
Dmytro Gnatyshyn <42860905+DG1202@users.noreply.github.com>
2019-08-20 23:31:48 +0300

    remove unnecessary code/comments

--- FL_11_16_homework_inheritance/homework/js/task3.js
+++ FL_11_16_homework_inheritance/homework/js/task3.js
@@ -64,36 +64,3 @@ pockemons.forEach(pockemon => {
   pockemon.prototype.constructor = pockemon;
 });
 
-const charmander = new Charmander();
-const charmeleon = new Charmeleon();
-const charizard = new Charizard();
-
-console.log(charmander.getType()); // -> “Fire”
-console.log(charmander.getType() === charmeleon.getType()); // -> true
-console.log(charmeleon.getType() === charizard.getType()); // -> true
-
-console.log(charmander.evolve().constructor === Charmeleon); // -> true
-console.log(charmeleon.evolve().constructor === Charizard); // -> true
-
-console.log(charmander.getSpecie()); // -> “Lizard Pokémon”
-console.log(charmeleon.getSpecie()); // -> “Flame Pokémon”
-console.log(charizard.getSpecie() === charmeleon.getSpecie()); // -> true
-
-console.log(charmander.canFly()); // -> false
-console.log(charmander.canFly() === charmeleon.canFly()); // -> true
-console.log(charizard.canFly()); // -> true
-
-
-const pichu = new Pichu();
-console.log(pichu.getPokemonType()); // => Pichu
-
-const pikachu = pichu.evolve();
-console.log(pikachu.getPokemonType()); // Pikachu
-console.log(pikachu.constructor === Pikachu); // true
-
-const raichu = pikachu.evolve();
-console.log(raichu.getPokemonType()); // Raichu
-console.log(raichu.constructor === Raichu); // true
-
-const raichu2 = raichu.evolve(); // return raichu back as it's maximum level
-console.log(raichu2 === raichu);
