014ff1b9cd6a31882496725c4962fc63e3a3c76d
Dmytro Gnatyshyn <42860905+DG1202@users.noreply.github.com>
2019-08-25 16:24:57 +0300

    final code formatting

--- FL_11_17_homework_es_next/homework/js/tasks.js
+++ FL_11_17_homework_es_next/homework/js/tasks.js
@@ -6,26 +6,26 @@ const copiedArray = (arr) => [...arr];
 
 //task3
 const addUniqueId = (obj) => {
-	const newObj = Object.assign({}, obj);
-	const uniqueId = Symbol('someUniqueId');
-	newObj.id = uniqueId;
-	return newObj
+  const newObj = Object.assign({}, obj);
+  const uniqueId = Symbol('someUniqueId');
+  newObj.id = uniqueId;
+  return newObj
 };
 
 //task4
 const regroupObject = (obj) => {
-	if(obj.name && obj.details.id && obj.details.age && obj.details.univercity) {
-		const {name: firstName, details: {id, age, univercity}} = obj;
-		return {
-			univercity,
-			user: {
-				id,
-				firstName,
-				age
-			}
-		}
+  if(obj.name && obj.details.id && obj.details.age && obj.details.univercity) {
+    const {name: firstName, details: {id, age, univercity}} = obj;
+      return {
+	univercity,
+	user: {
+	  id,
+	  firstName,
+	  age
 	}
-	throw Error('Wrong object ')
+      }
+  }
+  throw Error('Wrong object')
 };
 
 //task5
@@ -36,28 +36,28 @@ const hideNumber = (phoneNumber) => phoneNumber.slice(-4).padStart(phoneNumber.l
 
 //task7
 const isRequired = () => {
-	throw new Error('Missing property');
+  throw new Error('Missing property');
 };
 const add = (a = isRequired(), b = isRequired()) => a + b;
 
 //task8
 function fetchUsersNames(url) {
-	return fetch(url)
-		.then(response => response.json())
-		.then(users => console.log(users.map(user => user.name).sort()))
-		.catch(err => console.log(`ERROR: ${err.stack}`))
+  return fetch(url)
+    .then(response => response.json())
+    .then(users => console.log(users.map(user => user.name).sort()))
+    .catch(err => console.log(`ERROR: ${err.stack}`))
 }
 
 //task9
 async function fetchUsersNamesAsync(url) {
-	try {
-		const response = await fetch(url);
-		const users = await response.json();
-		let usersNames = users.map(user => user.name).sort();
-		console.log(usersNames);
-	} catch(err) {
-		console.log(err);
-	}
+  try {
+    const response = await fetch(url);
+    const users = await response.json();
+    let usersNames = users.map(user => user.name).sort();
+    console.log(usersNames);
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 
